e7a371e7901c539c79c0ec340a48407ec951c4fc
xaizek <xaizek@openmailbox.org>
2017-06-26 23:59:43 +0300

    Document decoration.hpp header

--- src/decoration.hpp
+++ src/decoration.hpp
@@ -22,68 +22,169 @@
 #include <iosfwd>
 #include <vector>
 
+/**
+ * @file decoration.hpp
+ *
+ * This unit provides facilities for decorating text with escape-sequences.
+ */
+
+/**
+ * @brief Terminal control manipulators for output streams.
+ *
+ * Usage example:
+ * @code
+ * std::cout << decor::bold << "This is bold text." << decor::def;
+ * @endcode
+ */
 namespace decor {
 
+/**
+ * @brief Type of function that performs decoration.
+ */
 using decorFunc = std::ostream & (*)(std::ostream &os);
 
+/**
+ * @brief Class describing single decoration or a combination of them.
+ */
 class Decoration
 {
 public:
+    /**
+     * @brief Constructs empty decoration.
+     */
     Decoration() = default;
+    /**
+     * @brief Constructs a (deep) copy of a decoration.
+     *
+     * @param rhs Original object to be copied.
+     */
     Decoration(const Decoration &rhs);
+    /**
+     * @brief Constructs decoration from a function.
+     *
+     * @param decorator Decorating function.
+     */
     explicit Decoration(decorFunc decorator);
+    /**
+     * @brief Constructs a decoration that is a combination of others.
+     *
+     * @param lhs First decoration to combine.
+     * @param rhs Second decoration to combine.
+     */
     Decoration(const Decoration &lhs, const Decoration &rhs);
-
+    /**
+     * @brief Move-assigns object's value.
+     *
+     * @param rhs Source of the move-assignment operation.
+     *
+     * @returns @c *this
+     */
     Decoration & operator=(Decoration &&rhs) = default;
 
 public:
+    /**
+     * @brief Actually performs the decoration of a stream.
+     *
+     * @param os Stream to decorate.
+     *
+     * @returns @p os.
+     */
     std::ostream & decorate(std::ostream &os) const;
 
 private:
+    //! Decoration function (can be nullptr).
     decorFunc decorator = nullptr;
+    //! One of two decorations that compose this object.
     std::unique_ptr<Decoration> lhs;
+    //! Second decoration that composes this object.
     std::unique_ptr<Decoration> rhs;
 };
 
+/**
+ * @brief Combines two decorations to create a new one.
+ *
+ * @param lhs First decoration to combine.
+ * @param rhs Second decoration to combine.
+ *
+ * @returns The combination.
+ */
 inline Decoration
 operator+(const Decoration &lhs, const Decoration &rhs)
 {
     return Decoration(lhs, rhs);
 }
 
+/**
+ * @brief A thunk for decoration use with @c std::ostream.
+ *
+ * @param os Stream to output to.
+ * @param d Decoration to apply.
+ *
+ * @returns @p os.
+ */
 inline std::ostream &
 operator<<(std::ostream &os, const Decoration &d)
 {
     return d.decorate(os);
 }
 
-extern const Decoration none;
-extern const Decoration bold;
-extern const Decoration inv;
-extern const Decoration def;
+/**
+ * @{
+ * @name Generic attributes
+ */
 
-extern const Decoration black_fg;
-extern const Decoration red_fg;
-extern const Decoration green_fg;
-extern const Decoration yellow_fg;
-extern const Decoration blue_fg;
-extern const Decoration magenta_fg;
-extern const Decoration cyan_fg;
-extern const Decoration white_fg;
+extern const Decoration none; //!< Convenience attribute that does nothing.
+extern const Decoration bold; //!< Enables bold attribute.
+extern const Decoration inv;  //!< Enables color inversion attribute.
+extern const Decoration def;  //!< Restores default attribute of the stream.
 
-extern const Decoration black_bg;
-extern const Decoration red_bg;
-extern const Decoration green_bg;
-extern const Decoration yellow_bg;
-extern const Decoration blue_bg;
-extern const Decoration magenta_bg;
-extern const Decoration cyan_bg;
-extern const Decoration white_bg;
+/**
+ * @}
+ *
+ * @{
+ * @name Foreground colors
+ */
+
+extern const Decoration black_fg;   //!< Picks black as foreground color.
+extern const Decoration red_fg;     //!< Picks red as foreground color.
+extern const Decoration green_fg;   //!< Picks green as foreground color.
+extern const Decoration yellow_fg;  //!< Picks yellow as foreground color.
+extern const Decoration blue_fg;    //!< Picks blue as foreground color.
+extern const Decoration magenta_fg; //!< Picks magenta as foreground color.
+extern const Decoration cyan_fg;    //!< Picks cyan as foreground color.
+extern const Decoration white_fg;   //!< Picks white as foreground color.
+
+/**
+ * @}
+ *
+ * @{
+ * @name Background colors
+ */
+
+extern const Decoration black_bg;   //!< Picks black as background color.
+extern const Decoration red_bg;     //!< Picks red as background color.
+extern const Decoration green_bg;   //!< Picks green as background color.
+extern const Decoration yellow_bg;  //!< Picks yellow as background color.
+extern const Decoration blue_bg;    //!< Picks blue as background color.
+extern const Decoration magenta_bg; //!< Picks magenta as background color.
+extern const Decoration cyan_bg;    //!< Picks cyan as background color.
+extern const Decoration white_bg;   //!< Picks white as background color.
+
+/**
+ * @}
+ *
+ * @{
+ * @name Control
+ */
 
 /**
  * @brief Forces disabling of decorations.
  */
 void disableDecorations();
+
+/**
+ * @}
+ */
 
 }
 
