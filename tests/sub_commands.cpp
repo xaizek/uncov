xaizek/uncov

commit 625b0654b497f2bde7c80e4c458d37a81e36b053

Fix incorrect parsing of optional build ids
These weren't parsed correctly:

    uncov changed @id /path
    uncov diff @id /path

src/sub_commands.cpp
@@ -594,7 +594,7 @@ private:
         BuildRef buildRef(bh);
         InRepoPath dirFilter(repo);
         boost::optional<Build> prevBuild;
-        if (auto parsed = tryParse<BuildId>(args)) {
+        if (auto parsed = tryParse<OptBuildId>(args)) {
             buildRef = std::get<0>(*parsed);
         } else if (auto parsed = tryParse<BuildId, BuildId>(args)) {
             BuildRef prevBuildRef(bh);

tests/sub_commands.cpp
@@ -612,6 +612,58 @@ test-file2.cpp   100.00%      2 / 2  +100.0000%    +2 /   -2 /    0
     CHECK(cerrCapture.get() == std::string());
 }
 
+TEST_CASE("Dirs without arguments uses latest build",
+          "[subcommands][dirs-subcommand]")
+{
+    Repository repo("tests/test-repo/subdir");
+    DB db(repo.getGitPath() + "/uncov.sqlite");
+    BuildHistory bh(db);
+
+    StreamCapture coutCapture(std::cout), cerrCapture(std::cerr);
+    CHECK(getCmd("dirs")->exec(getSettings(), bh, repo, "dirs",
+                               {}) == EXIT_SUCCESS);
+
+    const std::string expectedOut =
+R"(DIRECTORY  COVERAGE  C/R LINES  COV CHANGE  C/M/R LINE CHANGES
+/           100.00%      2 / 2   +50.0000%     0 /   -2 /   -2
+)";
+    CHECK(coutCapture.get() == expectedOut);
+    CHECK(cerrCapture.get() == std::string());
+}
+
+TEST_CASE("Changed lists files in specified build",
+          "[subcommands][changed-subcommand]")
+{
+    Repository repo("tests/test-repo/subdir");
+    DB db(repo.getGitPath() + "/uncov.sqlite");
+    BuildHistory bh(db);
+
+    StreamCapture coutCapture(std::cout), cerrCapture(std::cerr);
+    CHECK(getCmd("changed")->exec(getSettings(), bh, repo, "changed",
+                                  { "@@", "/test-file1.cpp" }) == EXIT_SUCCESS);
+
+    const std::string expectedOut =
+R"(FILE            COVERAGE  C/R LINES  COV CHANGE  C/M/R LINE CHANGES
+test-file1.cpp   100.00%      0 / 0  +100.0000%     0 /   -2 /   -2
+)";
+    CHECK(coutCapture.get() == expectedOut);
+    CHECK(cerrCapture.get() == std::string());
+}
+
+TEST_CASE("Diff fails when given one buildid and path",
+          "[subcommands][diff-subcommand]")
+{
+    Repository repo("tests/test-repo/subdir");
+    DB db(repo.getGitPath() + "/uncov.sqlite");
+    BuildHistory bh(db);
+
+    StreamCapture coutCapture(std::cout), cerrCapture(std::cerr);
+    CHECK(getCmd("diff")->exec(getSettings(), bh, repo, "diff",
+                               { "@@", "/test-file1.cpp" }) == EXIT_FAILURE);
+    CHECK(coutCapture.get() == std::string());
+    CHECK(cerrCapture.get() != std::string());
+}
+
 static SubCommand *
 getCmd(const std::string &name)
 {

src/arg_parsing.cpp
@@ -47,6 +47,17 @@ detail::parseArg<BuildId>::parse(const std::vector<std::string> &args,
             }
         }
     }
+    return { {}, ParseResult::Rejected};
+}
+
+auto
+detail::parseArg<OptBuildId>::parse(const std::vector<std::string> &args,
+                                    std::size_t idx)
+    -> std::pair<resultType, ParseResult>
+{
+    if (idx < args.size()) {
+        return parseArg<BuildId>::parse(args, idx);
+    }
     return { LatestBuildMarker, ParseResult::Skipped };
 }
 

src/arg_parsing.hpp
@@ -37,6 +37,8 @@
 
 //! Build id parameter (not defined).
 struct BuildId;
+//! Optional build id parameter (not defined).
+struct OptBuildId;
 //! Fiel path parameter (not defined).
 struct FilePath;
 //! Positive number parameter (not defined).
@@ -91,6 +93,27 @@ struct parseArg<BuildId>
 
     /**
      * @brief Parses build id.
+     *
+     * @param args Input arguments.
+     * @param idx  Current position within @p args or just past its end.
+     *
+     * @returns Parsed value and indication whether parsing was successful.
+     */
+    static std::pair<resultType, ParseResult>
+    parse(const std::vector<std::string> &args, std::size_t idx);
+};
+
+/**
+ * @brief Parses optional build id, which is of the form `/@@|@number/`.
+ */
+template <>
+struct parseArg<OptBuildId>
+{
+    //! Type of result yielded by this parser.
+    using resultType = boost::variant<int, std::string>;
+
+    /**
+     * @brief Parses optional build id.
      *
      * @param args Input arguments.
      * @param idx  Current position within @p args or just past its end.

commit d974f566d93fd647cb82c1245ed93383712ba135

Fix `uncov build` (no args o subcmd) not working
Got broken by 625b0654b497f2bde7c80e4c458d37a81e36b053:

    Fix incorrect parsing of optional build ids

There might be other commands that need to be updated to use `OptBuildId`
instead of `BuildId`.

src/sub_commands.cpp
@@ -260,7 +260,7 @@ private:
              const std::vector<std::string> &args) override
     {
         BuildRef buildRef(bh);
-        if (auto parsed = tryParse<BuildId>(args)) {
+        if (auto parsed = tryParse<OptBuildId>(args)) {
             std::tie(buildRef) = *parsed;
         } else {
             std::cerr << "Invalid arguments for subcommand.\n";

tests/sub_commands.cpp
@@ -95,6 +95,17 @@ private:
 
 static SubCommand * getCmd(const std::string &name);
 
+const std::string build3info =
+R"(Id:                  #3                                      
+Coverage:            100.00%                                 
+C/R Lines:           2 / 2                                   
+Cov Change:          +50.0000%                               
+C/M/R Line Changes:  0 / -2 / -2                             
+Ref:                 master                                  
+Commit:              d1b12454989580b470be93e71cc60c2e32fd5889
+Time:                2017-01-09 15:17:51                     
+)";
+
 TEST_CASE("Error on wrong branch", "[subcommands][build-subcommand]")
 {
     Repository repo("tests/test-repo/subdir");
@@ -121,6 +132,20 @@ TEST_CASE("Invalid arguments for build", "[subcommands][build-subcommand]")
     CHECK(cerrCapture.get() == "Invalid arguments for subcommand.\n");
 }
 
+TEST_CASE("Build information on last build", "[subcommands][build-subcommand]")
+{
+    Repository repo("tests/test-repo/subdir");
+    DB db(repo.getGitPath() + "/uncov.sqlite");
+    BuildHistory bh(db);
+
+    StreamCapture coutCapture(std::cout), cerrCapture(std::cerr);
+    CHECK(getCmd("build")->exec(getSettings(), bh, repo, "build",
+                                { }) == EXIT_SUCCESS);
+
+    CHECK(coutCapture.get() == build3info);
+    CHECK(cerrCapture.get() == std::string());
+}
+
 TEST_CASE("Build information on a branch", "[subcommands][build-subcommand]")
 {
     Repository repo("tests/test-repo/subdir");
@@ -131,17 +156,7 @@ TEST_CASE("Build information on a branch", "[subcommands][build-subcommand]")
     CHECK(getCmd("build")->exec(getSettings(), bh, repo, "build",
                                 { "@master" }) == EXIT_SUCCESS);
 
-    const std::string expectedOut =
-R"(Id:                  #3                                      
-Coverage:            100.00%                                 
-C/R Lines:           2 / 2                                   
-Cov Change:          +50.0000%                               
-C/M/R Line Changes:  0 / -2 / -2                             
-Ref:                 master                                  
-Commit:              d1b12454989580b470be93e71cc60c2e32fd5889
-Time:                2017-01-09 15:17:51                     
-)";
-    CHECK(coutCapture.get() == expectedOut);
+    CHECK(coutCapture.get() == build3info);
     CHECK(cerrCapture.get() == std::string());
 }
 