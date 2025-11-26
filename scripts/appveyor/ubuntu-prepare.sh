3d7ab863e02678d7c1f1da329827f7ebf674cd8b
xaizek <xaizek@posteo.net>
2025-11-26 14:59:51 +0200

    Improve scripts/appveyor/ubuntu-prepare.sh
    Use long version of `-y` passed to `apt install` and add `--quiet`
    (doesn't seem to have an effect) and `--no-install-recommends`.

--- scripts/appveyor/ubuntu-prepare.sh
+++ scripts/appveyor/ubuntu-prepare.sh
@@ -2,12 +2,13 @@
 
 set -xe
 
-sudo apt install -y libboost-filesystem-dev \
-                    libboost-iostreams-dev \
-                    libboost-program-options-dev \
-                    libboost-system-dev \
-                    libgit2-dev \
-                    libsource-highlight-dev \
-                    libsqlite3-dev \
-                    libtntnet-dev \
-                    ccache
+sudo apt install --quiet --assume-yes --no-install-recommends \
+                 libboost-filesystem-dev \
+                 libboost-iostreams-dev \
+                 libboost-program-options-dev \
+                 libboost-system-dev \
+                 libgit2-dev \
+                 libsource-highlight-dev \
+                 libsqlite3-dev \
+                 libtntnet-dev \
+                 ccache
